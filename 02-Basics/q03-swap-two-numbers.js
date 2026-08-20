ff63deacd61dfad863af5912bc2dd6d8ee7e21f0
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-20 19:02:31 +0530

    solved swap two numbers

---
+++ 02-Basics/q03-swap-two-numbers.js
@@ -0,0 +1,30 @@
+// Q.3 Swap Two Numbers:
+
+    // let a = 10;
+    // let b = 20;
+    // let temp = a;
+
+    // a = b;
+    // b = temp;
+
+    // console.log(a, b);
+
+// ------------------ or ----------------
+
+    // let a = 10;
+    // let b = 20;
+
+    // [a, b] = [b, a];
+
+    // console.log(a, b);
+
+// ------------------ or ----------------
+
+    // let a = 10;
+    // let b = 20;
+
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+
+    // console.log(a, b);
