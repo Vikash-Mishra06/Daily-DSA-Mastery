394b344bc5d8a4b6354e2404d3b8fe0cf443c11a
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-20 18:47:41 +0530

    solved increasing & decreasing star pattern

---
+++ 01-Patterns/Star-Patterns/q01-star-pattern-5-to-1.js
@@ -0,0 +1,17 @@
+// Q.1 Star pattern 5 to 1:
+
+    // for (let i = 5; i >=1; i--) {
+    //     console.log('*'.repeat(i))
+    // }
+
+// ------------------ or ----------------
+
+    // for (let i = 5; i >= 1; i--) {
+    //     let stars = "";
+
+    //     for (let j = 1; j <= i; j++) {
+    //         stars += "*";
+    //     }
+
+    //     console.log(stars);
+    // }
---
+++ 01-Patterns/Star-Patterns/q02-star-pattern-1-to-5.js
@@ -0,0 +1,16 @@
+// Q.2 Print the following pattern from 1 to 5 for n = 5:
+
+    // for (let i = 1; i <=5; i++) {
+    //     console.log('*'.repeat(i));
+    // }
+
+// ------------------ or ----------------
+
+    // for (let i = 1; i <=5; i++) {
+    //     let star = "";
+
+    //     for (let j = 1; j <= i; j++) {
+    //         star += "*";
+    //     }
+    //     console.log(star);
+    // }
