b143279099ebbe10a1e73b8105dc45d2374e3b67
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-21 20:56:53 +0530

    solved reverse words

---
+++ 04-Strings/q05-reverse-words.js
@@ -0,0 +1,19 @@
+// Q5 — Reverse Words in a String:
+
+    // function reverseWords(str) {
+    //     let arr = str.split(" ");
+    //     let left = 0;
+    //     let right = arr.length-1;
+
+    //     while (left < right) {
+    //         let temp = arr[left];
+    //         arr[left] = arr[right];
+    //         arr[right] = temp;
+
+    //         left++;
+    //         right--;
+    //     }
+    //     return arr.join(" ");
+    // }
+
+    // console.log(reverseWords('Vikash Mishra'));
