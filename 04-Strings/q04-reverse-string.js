f5d3d391efe90fbf420311255a48c3605d964a58
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-21 16:14:44 +0530

    solved reverse string

---
+++ 04-Strings/q04-reverse-string.js
@@ -0,0 +1,32 @@
+// Q.4 Reverse a String:
+
+    // function reverseString(str) {
+    //     let reverse = "";
+
+    //     for (let i = str.length-1; i >= 0; i--) {
+    //         reverse += str[i];
+    //     }
+    //     return reverse;
+    // }
+
+    // console.log(reverseString("vikash"));
+
+// ------------------ or ----------------
+    
+    // function reverseString(str) {
+    //     let arr = str.split('');
+    //     let left = 0;
+    //     let right = str.length-1;
+
+    //     while (left < right) {
+    //         let temp = arr[left];
+    //         arr[left] = arr[right];
+    //         arr[right] = temp;
+
+    //         left++;
+    //         right--;
+    //     }
+    //     return arr.join('');
+    // }
+
+    // console.log(reverseString('vikash'));
