a4b3292ffeeb7f2ccda17c4219f45cb58502b7a0
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-23 11:21:56 +0530

    solved most frequent character

---
+++ 04-Strings/q08-most-frequent-character.js
@@ -0,0 +1,19 @@
+// Q8 — Most Frequent Character
+
+    // function mostFreq(str) {
+    //     let map = {};
+    //     let max = 0;
+    //     let result = null;
+
+    //     for (let ch of str) {
+    //         map[ch] = (map[ch] || 0) + 1;
+
+    //         if (map[ch] > max) {
+    //             max = map[ch];
+    //             result = ch;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    // console.log(mostFreq('hello'));
