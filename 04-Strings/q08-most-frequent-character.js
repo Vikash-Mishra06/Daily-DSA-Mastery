ffd4df6b0127e75285258d5fd8cd4dba05099692
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-23 11:53:57 +0530

    Added another solution for Most Frequent from String problem.

--- 04-Strings/q08-most-frequent-character.js
+++ 04-Strings/q08-most-frequent-character.js
@@ -17,3 +17,24 @@
     // }
 
     // console.log(mostFreq('hello'));
+
+// ------------------ or --------------------
+
+    // function mostFreq(str) {
+    //     let map = new Map();
+    //     let max = 0;
+    //     let result = null;
+
+    //     for (let ch of str) {
+    //         map.set(ch, (map.get(ch) ||0) +1);
+
+    //         if (map.get(ch) > max) {
+    //             max = map.get(ch);
+    //             result = ch;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+
+    // console.log(mostFreq('Javascript'));
