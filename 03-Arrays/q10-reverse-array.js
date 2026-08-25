b9995e3e72e1e4ad1d0fdc63dfa406ae6b61b0fd
Vikash Mishra <mishravikash0612@gmail.com>
2026-08-25 21:35:46 +0530

    solved reverse array

---
+++ 03-Arrays/q10-reverse-array.js
@@ -0,0 +1,31 @@
+// Q10 — Reverse an Array
+
+    // function reverseArray(arr) {
+    //     let left = 0;
+    //     let right = arr.length-1;
+
+    //     while (left < right) {
+    //         let temp = arr[left];
+    //         arr[left] = arr[right];
+    //         arr[right] = temp;
+
+    //         left++;
+    //         right--;
+    //     }
+    //     return arr;
+    // }
+
+    // console.log(reverseArray([1,2,3,4,5]));
+
+// --------------------- or --------------------------
+
+    // function reverseArray(arr) {
+    //     let reversed = [];
+
+    //     for (let i = arr.length-1; i >= 0; i--) {
+    //         reversed.push(arr[i]);
+    //     }
+    //     return reversed;
+    // }
+
+    // console.log(reverseArray([1,2,3,4,5]));
